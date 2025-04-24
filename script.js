cadeec208a7e611f50d7ad057899c2a36805a93e
Ambs04 <amberleejones2007@gmail.com>
2025-04-24 21:44:39 +0200

    add functionality to see more btn

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
 const btn = document.querySelector("#btn-div button");
 const spinner = document.getElementById("spinner");
+
+//show spinner on clik of See more button
+btn.addEventListener("click", function (e) {
+  //prevent navigation happening immediately
+  e.preventDefault();
+
+  //show spinner
+  spinner.classList.remove("hidden");
+
+  btn.style.cursor = "wait";
+
+  //set delay
+  setTimeout(() => {
+    //open url in new tab
+    window.open("https://github.com/Ambs04", "_blank");
+    spinner.classList.add("hidden");
+  }, 2000);
+});
